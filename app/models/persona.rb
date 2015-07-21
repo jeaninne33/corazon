0ad853643ac18f12f16b6cb9bd95fd3221ba620b
kw0lf <vicedeno@gmail.com>
2015-07-21 08:10:27 -0430

    cambios

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,6 +1,11 @@
 class Persona < ActiveRecord::Base
 	has_one :economy, dependent: :destroy
 	has_many :conclusions, dependent: :destroy
-	accepts_nested_attributes_for :economy, :conclusions
+	accepts_nested_attributes_for :economy
+    accepts_nested_attributes_for :conclusions,
+	 reject_if: proc { |attributes| attributes['solicitud'].blank? }, 
+	 :allow_destroy => true
+
+
 	
 end
