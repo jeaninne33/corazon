1a9d757c1c0ac94761ae9af7ebedc0b86d02da3d
victor <victor@Odin>
2015-07-24 15:23:49 -0430

    aaaaaaaaaaaa

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :personas
   get 'personas/:id/listado' => 'personas#listado'
   get 'personas/:id/totales' => 'personas#totales'
-
+  get 'personas/:id/totales_municipio' => 'personas#totales_municipio'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
