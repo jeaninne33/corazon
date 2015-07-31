b1b63b97192042592cb4116df4c2a0d6ef71af8b
kw0lf <nethax0r@gmail.com>
2015-07-31 13:53:21 -0430

    ultimos cambios

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -3,9 +3,9 @@ class Persona < ActiveRecord::Base
 	has_many :conclusions, dependent: :destroy
 	accepts_nested_attributes_for :economy
     accepts_nested_attributes_for :conclusions,
-	 reject_if: proc { |attributes| attributes['solicitud'].blank? }, 
+	 reject_if: proc { |attributes| attributes.all? { |k, v| v.blank? }} ,
 	 :allow_destroy => true
-validates :cedula , uniqueness: true
+	validates :cedula , uniqueness: true
 
 	
 end
